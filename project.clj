5828d288846acc793b8a15a1fb8c4c21f24e42a9
John Crooks <johnmcrooks@gmail.com>
2016-07-06 14:59:07 -0400

    added web functionality, sorting by category

--- project.clj
+++ project.clj
@@ -2,7 +2,10 @@
   :description "FIXME: write description"
   :plugins [[io.aviso/pretty "0.1.26"]]
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [io.aviso/pretty "0.1.26"]]
+                 [io.aviso/pretty "0.1.26"]
+                 [ring "1.4.0"]   
+                 [hiccup "1.0.5"]  ;clojure equivalent of mustache
+                 [compojure "1.5.0"]] ; allows you to write routes
   :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
   :aot [purchases-closure.core]
   :main purchases-closure.core)
--- src/purchases_closure/core.clj
+++ src/purchases_closure/core.clj
@@ -1,24 +1,65 @@
 (ns purchases-closure.core
-  (:require [clojure.string :as str])
+  (:require [clojure.string :as str]
+            [compojure.core :as c]
+            [ring.adapter.jetty :as j]; jetty is a commonly used java webserver 
+            [hiccup.core :as h])  ; almost the equivalent of mustache])
   (:gen-class))
 
-(defn -main []
+
+
+(defn purchases-html [purchases]
+  [:html
+   [:body
+    [:a {:href "/"} "|  All  |"
+     [:a {:href "/Food"} "  Food  |"]]  ; how to create a hyperlink and specify attributes within a tag (they go in the hashmap... HINT HINT)
+    [:a {:href "/Alcohol"} "  Alcohol  |"] 
+    [:a {:href "/Furniture"} "  Furniture  |"] 
+    [:a {:href "/Shoes"} "  Shoes  |"] 
+    [:a {:href "/Toiletries"} "  Toiletries  |"]
+    [:a {:href "/Jewelry"} "  Jewelry  |"] 
+   
+    [:ul {:style "list-style-type:none"}            ;creating an organized list
+     (map (fn [purchase]        
+            [:li (str (get purchase "customer_id") ": " (get purchase "date") " " (get purchase "credit_card") " " (get purchase "cvv") " " (get purchase "category"))])  ;grabbing and combining just the first and last names from the person hashmap and combining them with 'str'
+       purchases)]]])
+           
+;        uinput (read-line)
+;        purchases(filter(fn [line] (= (get line "category") uinput)) ;Alternative way of writing this function.... purchases(filter #(= input (get % "category")) purchases)
+;                   purchases)
+;        file-text (pr-str purchases)]
+;    (spit "filtered_purchases.edn" file-text)))
+
+(defn read-purchases []
   (let [purchases (slurp "purchases.csv")
         purchases (str/split-lines purchases)
-        purchases (map (fn [line] (str/split line #","))
+        purchases (map (fn [line] (str/split line #","))  ;Alternative way of writing this line.... purchases (map #(str/split % #",") purchases)
                     purchases)
         header (first purchases)
         purchases (rest purchases)
         purchases (map(fn [line] (zipmap header line))
-                    purchases)
-        _ (println "What would you like to filter by? [Furniture, Alcohol, Toiletries, Shoes, Food, Jewelry]")
-        uinput (read-line)
-        purchases(filter(fn [line] (= (get line "category") uinput))
-                   purchases)
-        file-text (pr-str purchases)]
-       
-    (spit "filtered_purchases.edn" file-text)))
+                    purchases)]
+    purchases))
             
-            
+(defn filter-by-category [purchases category]
+  (filter( fn [purchase]
+           (= category (get purchase "category")))
+    purchases))
+
+(c/defroutes app
+  (c/GET "/:category{.*}" [category]
+    (let [purchases (read-purchases)
+          purchases (if (= 0 (count category))
+                     purchases
+                     (filter-by-category purchases category))]
+      (h/html (purchases-html purchases)))))
+ 
+(defonce server (atom nil))
+
+(defn -main[]
+  (if @server
+    (.stop @server))   
+  (reset! server  
+    (j/run-jetty app {:port 3000 :join? false})))  
+              
 ;
         
